a3930916c62497d120561079a58b8f7688fb246d
Yoseph Winata <yoseph.winata@gmail.com>
2020-05-13 21:23:46 +0700

    resizing oversized images

--- app/assets/js/cms/featured_menu.js
+++ app/assets/js/cms/featured_menu.js
@@ -2,47 +2,47 @@ const featuredMenu = {
   featuredMenuList: [
     {
       name: "Ayam Goreng Terasi",
-      image: "ayam_goreng_terasi.jpeg",
+      image: "ayam_goreng_terasi.jpg",
       price: 49
     },
     {
       name: "Buncis Sapi Sichuan",
-      image: "buncis_sapi_sichuan.jpeg",
+      image: "buncis_sapi_sichuan.jpg",
       price: 49
     },
     {
       name: "Kangkung Hungry Panda",
-      image: "kangkung_special_hp.JPG",
+      image: "kangkung_special_hp.jpg",
       price: 39
     },
     {
       name: "Nasi Gorng Yang Chow",
-      image: "nasi_goreng_yangchow.JPG",
+      image: "nasi_goreng_yangchow.jpg",
       price: 39
     },
     {
       name: "Sapi Lada Hitam",
-      image: "sapi_lada_hitam.jpeg",
+      image: "sapi_lada_hitam.jpg",
       price: 59
     },
     {
       name: "Polo Pao Panggang",
-      image: "polopao_panggang.jpeg",
+      image: "polopao_panggang.jpg",
       price: 19
     },
     {
       name: "Onde Wijen Hitam",
-      image: "onde_wijen_hitam.JPG",
+      image: "onde_wijen_hitam.jpg",
       price: 19
     },
     {
       name: "Hakau",
-      image: "hakau.jpeg",
+      image: "hakau.jpg",
       price: 19
     },
     {
       name: "Siew Mai",
-      image: "siew_mai.jpeg",
+      image: "siew_mai.jpg",
       price: 19
     }
   ]
